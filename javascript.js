54630fb0c116ed6f7d8dbe0a617a49776a31f449
Jacob Welander Jensen <64834767+Welander1994@users.noreply.github.com>
2021-03-11 17:47:55 +0100

    styling
    viser med farver om budget er i minus eller i plus

--- javascript.js
+++ javascript.js
@@ -6,17 +6,25 @@ function add() {
     indkomst = Number(document.getElementById("indkomst").value);
     husleje = Number(document.getElementById("husleje").value);
     mad = Number(document.getElementById("mad").value);
-
+ 
     Budget = indkomst - husleje - mad;
 
     udgifter = husleje + mad;
     
-    document.getElementById("Budget").innerHTML= Budget;
-    document.getElementById("udgifter").innerHTML= udgifter;
+    document.getElementById("Budget").innerHTML=  Budget;
+    document.getElementById("udgifter").innerHTML= "-" + udgifter;
 
     console.log("Indkomst = " + indkomst);
     console.log("husleje = " + husleje);
     console.log("mad = " + mad);
     console.log("Budget = " + Budget);
     console.log("udgifter = " + udgifter);
+
+    if(0 > Budget ) {
+        document.getElementById("Budget").style.color = "red";
+    }
+    else {
+        document.getElementById("Budget").style.color = "green";
+        
+    }
 }
