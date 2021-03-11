99efd9b6078654cb56f93898aa0b601e4710cf3a
Jacob Welander Jensen <64834767+Welander1994@users.noreply.github.com>
2021-03-11 21:36:07 +0100

    add more inputs and styling

--- javascript.js
+++ javascript.js
@@ -1,30 +1,102 @@
-console.log("Detter er din budget regner")
+console.log("Dette er din budget regner")
 
 function add() {
-    var indkomst,husleje,Budget,mad,udgifter;
-    indkomst = Number(document.getElementById("indkomst").value);
+    var indkomst,
+    su,
+    job,
+    fasteudgifter,
+    VariableUdgifter,
+    husleje,Budget,
+    mad,
+    transport,
+    fagforening,
+    forsikringer,
+    mobil,
+    internet,
+    licens,
+    streaming,
+    tøj,
+    cafe,
+    uforudset,
+    opsparing;
+
+    /*indkomst */
+    job = Number(document.getElementById("job").value);
+    su = Number(document.getElementById("su").value);
+
+    /*faste udgifter*/
     husleje = Number(document.getElementById("husleje").value);
+    transport = Number(document.getElementById("transport").value);
+    fagforening = Number(document.getElementById("fagforening").value);
+    forsikringer = Number(document.getElementById("forsikringer").value);
+    mobil = Number(document.getElementById("mobil").value);
+    internet = Number(document.getElementById("internet").value);
+    licens = Number(document.getElementById("licens").value);
+    streaming = Number(document.getElementById("streaming").value);
+
+
+    /*Variable udgifter*/
     mad = Number(document.getElementById("mad").value);
- 
-    Budget = indkomst - husleje - mad;
+    tøj = Number(document.getElementById("tøj").value);
+    cafe = Number(document.getElementById("cafe").value);
+    uforudset = Number(document.getElementById("uforudset").value);
+    opsparing = Number(document.getElementById("opsparing").value);
 
-    udgifter = husleje + mad;
+    /*Variable udgifter*/
+    indkomst = su + job;
+
+    /*ændre senere*/
+    fasteudgifter = husleje + transport + fagforening + forsikringer + mobil + internet + licens + streaming;
+
+    /*Variable udgifter*/
+    VariableUdgifter = mad + tøj + cafe + uforudset + opsparing;
+
+     /*ændre senere*/
+    Budget = indkomst - fasteudgifter - VariableUdgifter;
     
-    document.getElementById("Budget").innerHTML=  Budget;
-    document.getElementById("udgifter").innerHTML= "-" + udgifter;
+    document.getElementById("indkomst").innerHTML=  indkomst;
+    document.getElementById("budget").innerHTML=  Budget;
+    document.getElementById("fasteudgifter").innerHTML= "-" + fasteudgifter;
+    document.getElementById("variableUdgifter").innerHTML= "-" + VariableUdgifter;
 
+    /*indkomst*/
     console.log("Indkomst = " + indkomst);
-    console.log("husleje = " + husleje);
-    console.log("mad = " + mad);
-    console.log("Budget = " + Budget);
-    console.log("udgifter = " + udgifter);
+    console.log("SU = " + su);
+    console.log("Job = " + job);
 
+    /*faste udgifter*/
+    console.log("husleje = " + husleje);
+    console.log("transport = " + transport);
+    console.log("fagforening = " + fagforening);
+    console.log("forsikringer = " + forsikringer);
+    console.log("mobil = " + mobil);
+    console.log("internet = " + internet);
+    console.log("licens = " + licens);
+    console.log("streaming = " + streaming);
+    
+
+    /*Variable udgifter*/
+    console.log("mad = " + mad);
+    console.log("tøj = " + tøj);
+    console.log("cafe = " + cafe);
+    console.log("uforudset udgifter = " + uforudset);
+    console.log("opsparing = " + opsparing);
+  
+
+    /*samlet udregninger*/
+    console.log("Budget = " + Budget);
+    console.log("Faste udgifter = " + fasteudgifter);
+    console.log("Variable udgifter = " + VariableUdgifter);
+
+
+    /*
     if(0 > Budget ) {
-        document.getElementById("Budget").style.color = "red";
+        document.getElementById("budget").style.color = "red";
     }
     else {
-        document.getElementById("Budget").style.color = "green";
+        document.getElementById("budget").style.color = "green";
         
     }
+    */
 }
 
