71bf0fd5e2a8f69bd9564fcf1417a68f99a2d911
Jacob Welander Jensen <64834767+Welander1994@users.noreply.github.com>
2021-03-13 22:42:42 +0100

    1

--- test.js
+++ test.js
@@ -1,27 +1,61 @@
-function add() {
-    newInput = document.getElementById("newInput").value;
+function addIndtægt() {
+    newIndtægt = document.getElementById("addIndtægt").value;
 
     
     var input = document.createElement("input"); 
     input.type = "text"
-    input.name = newInput;
-    input.id = newInput;
-    input.placeholder = newInput;   
-
-
+    input.name = newIndtægt;
+    input.id = newIndtægt;
+    input.placeholder = newIndtægt;   
 
     var p = document.createElement("p"); 
 
-    document.getElementById("test").appendChild(input);  
-    document.getElementById("test").appendChild(p);  
-    console.log(newInput); 
+    document.getElementById("indtægt").appendChild(input);  
+    document.getElementById("indtægt").appendChild(p);   
 }
 
-function myFunction() {
-var x = document.getElementById("test").getElementsByTagName("input");
-for (var i = 0; i < x.length; i++) {
-   x[i] = Number(document.getElementById("job").value);
-   console.log(x[i].name, "+", x[i].value);
+function addudgifter() {
+    newUdgifter = document.getElementById("addudgifter").value;
+
+    
+    var input = document.createElement("input"); 
+    input.type = "text"
+    input.name = newUdgifter;
+    input.id = newUdgifter;
+    input.placeholder = newUdgifter;   
+
+    var p = document.createElement("p"); 
+
+    document.getElementById("udgifter").appendChild(input);  
+    document.getElementById("udgifter").appendChild(p);   
 }
 
+function cal() {
+    var samletindtægt = 0;
+    var indtægt = document.getElementById("indtægt").getElementsByTagName("input");
+    for (var i = 0; i < indtægt.length; i++) {
+    
+        console.log(indtægt[i].name, "+", indtægt[i].value);
+        samletindtægt += parseInt(indtægt[i].value);
+        
 }
+    var samletudgift = 0;
+    var udgifter = document.getElementById("udgifter").getElementsByTagName("input");
+    for (var i = 0; i < udgifter.length; i++) {
+
+        
+        console.log(udgifter[i].name, "+", udgifter[i].value);
+        samletudgift += parseInt(udgifter[i].value);
+        
+}
+    console.log("samletindtægt " + samletindtægt);
+    console.log("samletudgift " + samletudgift);
+    var budget = samletindtægt - samletudgift;
+    var årligomkostninger = samletudgift * 12;
+    console.log("budget " + budget);
+    document.getElementById("samletindtægt").innerHTML = samletindtægt;
+    document.getElementById("samletudgift").innerHTML = samletudgift;
+    document.getElementById("budget").innerHTML = budget;
+    document.getElementById("årligomkostninger").innerHTML = årligomkostninger;
+}
+
